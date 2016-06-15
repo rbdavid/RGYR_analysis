72fb3c0fad21f97c8cb0ec49b064258f8c0a21a6
rbdavid <russell.davidson@colostate.edu>
2016-06-14 20:27:45 -0600

    fixed some bugs in plotting script

--- rgyr_plotting.py
+++ rgyr_plotting.py
@@ -3,7 +3,7 @@
 
 # PREAMBLE:
 
-from fn_plotting import *
+from plotting_functions import *
 from sel_list import *
 
 stdev = np.std
@@ -25,9 +25,9 @@ time = np.zeros(nSteps)
 for i in range(nSteps):
 	time[i] = i*0.002		# units of time in ns; each frame is separated by 0.002 ns 
 
-for i in range(len(sel)):
+for i in range(len(selection)):
 	system = selection[i][0]
 	num = i
 
-	scat_hist(time[:], datalist1[:,i], 'k', 'Time', 'RGYR ($\AA$)', '%02d.%s.%s' %(num, system1, system), 'RGYR',xunits='ns',yunits='$\AA$',)
+	scat_hist(time[:], datalist1[:,i], 'k', 'Time', 'RGYR', '%02d.%s.%s' %(num, system1, system),'RGYR',xunits='ns',yunits='$\AA$')
 
