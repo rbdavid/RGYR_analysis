bcb2ed31063ceebeedd3f62f880e62839761142b
Russell Bruce Davidson <Russell.Davidson@colostate.edu>
2016-10-25 10:56:40 -0600

    updated the TO_DO file as well as added a new atom selection for the ADP substrate

--- sel_list.py
+++ sel_list.py
@@ -2,6 +2,7 @@
 selection = []
 selection.append(['prot','protein'])
 selection.append(['prot-12','protein and not (resid 0:12)'])
-selection.append(['ssRNA','nucleic or (resname A5 or resname A3 or resname U5) and not (resname atp or resname adp)'])
+selection.append(['ssRNA','nucleic or (resname A5 A3 U5) and not (resname atp adp)'])
 selection.append(['ATP','resname atp'])
+#selection.append(['ADP','resname adp'])
 
